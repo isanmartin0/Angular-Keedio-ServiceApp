b97cc9946ce483722cd55c6ad27aad034380c57c
isanmartin <isanmartin@keedio.com>
2018-09-21 11:00:45 +0200

    Added uat environment

--- src/app/hello/hello.component.spec.ts
+++ src/app/hello/hello.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HelloComponent } from './hello.component';
 
-fdescribe('HelloComponent', () => {
+describe('HelloComponent', () => {
   let component: HelloComponent;
   let fixture: ComponentFixture<HelloComponent>;
 
