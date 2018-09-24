719afe3569a2aeb56bac03e107cc890b18dce066
isanmartin <isanmartin@keedio.com>
2018-09-24 05:03:05 +0200

    Focus on hello tests

--- src/app/hello/hello.component.spec.ts
+++ src/app/hello/hello.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HelloComponent } from './hello.component';
 
-describe('HelloComponent', () => {
+fdescribe('HelloComponent', () => {
   let component: HelloComponent;
   let fixture: ComponentFixture<HelloComponent>;
 
